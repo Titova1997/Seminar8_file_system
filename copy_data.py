c1864916ce55b979ce55704622f094d422aa2aff
Titova1997 <152087142+Titova1997@users.noreply.github.com>
2024-05-16 17:38:30 +0300

    Add files via upload

---
+++ copy_data.py
@@ -0,0 +1,22 @@
+from return_data_file import data_file
+
+
+def copy_file():
+     data, nf = data_file()
+     answer = int(input("Выберите файл, в который Вы хотите"
+                        "перенести данные (от 1 до 2): "))
+     while (answer < 1 or answer > 2) or answer == nf:
+          answer = int(input("ОШИБКА! Вы ввели некоректное значение, или"
+                            "выбрали тот же файл\n"
+                            "Выберите файл, в который Вы хотите"
+                            "перенести данные (от 1 до 2): "))
+     with open (f'db/data_{answer}.txt','r', encoding='utf-8') as file:
+        count_row_data_end = len (file.readlines()) + 1
+        data = [f'{i + count_row_data_end};{data[i].split(";")[1]};'
+                f'{data[i].split(";")[2]};'
+                f'{data[i].split(";")[3]};'
+                f'{data[i].split(";")[4]}'
+                for i in range(len(data))]
+        with open (f'db/data_{answer}.txt','a', encoding='utf-8') as file:
+            file.writelines(data)
+        print("Данные успешно скопированны!")
